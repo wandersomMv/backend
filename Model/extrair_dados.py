e191e0a7e1b43bc0606548da4e7b015cc66b19b7
Phones <paulohenriqueh178h@gmail.com>
2020-02-29 16:57:59 -0300

    Adição da classe (dados_planilhas) e criação do dicionario de dados

--- Model/extrair_dados.py
+++ Model/extrair_dados.py
@@ -3,44 +3,15 @@ import untangle
 import xmltodict
 import datetime
 from time import sleep
+from Model.dados_planilhs import dados
 from xml.etree import ElementTree as et
 
 class Extracao_dados:
-    def __init__(self,
-        convenio = '', 
-        data_pagamento = '', 
-        numero_protocolo = '', 
-        matricula = '', 
-        nome = '', 
-        numero_guia = '', 
-        ng_prest = '', 
-        senha_guia = '', 
-        codigo_produto = '', 
-        descricao_produto = '', 
-        valor_apresentado = '', 
-        valor_pago = '', 
-        valor_glosa = '', 
-        descricao_motivo = '', 
-        codigo_motivo = ''
-    ):
-        self.convenio = convenio
-        self.data_pagamento = data_pagamento
-        self.numero_protocolo = numero_protocolo
-        self.matricula = matricula
-        self.nome = nome
-        self.numero_guia = numero_guia
-        self.ng_prest = ng_prest
-        self.senha_guia = senha_guia
-        self.codigo_produto = codigo_produto
-        self.descricao_produto = descricao_produto
-        self.valor_apresentado = valor_apresentado
-        self.valor_pago = valor_pago
-        self.valor_glosa = valor_glosa
-        self.descricao_motivo = descricao_motivo
-        self.codigo_motivo = codigo_motivo
+    def __init__(self):
         self.nome_arquivos = []
         self.caminho_ate_pasta_downloads = ''
-        self.lista_objetos = []
+        self.lista_num_prestes = []
+        self.dicionario_chave_num_prestes = {}
 
 
     
@@ -61,11 +32,11 @@ class Extracao_dados:
         # EXTRAI OS DADOS, E RETORNA A TUPLA COM ELES
         return tuple(doc['data']['row'])
 
-    # MONTA A TUPLA, COM OS OBJETOS PREENCHIDOS, E CADA OBJETO EQUIVALE A UMA LINNHA DA PLANILHA EM XML
-    def monta_tupla_de_objetos(self,nome_arquivo):
+    # MONTA O DICIONARIO, COM OS OBJETOS PREENCHIDOS, E CADA OBJETO EQUIVALE A UMA LINNHA DA PLANILHA EM XML
+    def monta_diacionario_de_objetos(self,nome_arquivo):
         tupla_de_dados = self.retorna_tupla_dados(nome_arquivo)
         for tupla_dado in tupla_de_dados:
-            objeto = Extracao_dados(
+            objeto = dados(
                 convenio = tupla_dado['convenio'], 
                 data_pagamento = tupla_dado['data_pagamento'], 
                 numero_protocolo = tupla_dado['numero_protocolo'], 
@@ -82,28 +53,48 @@ class Extracao_dados:
                 descricao_motivo = tupla_dado['descricao_motivo'], 
                 codigo_motivo = tupla_dado['codigo_motivo'],
             )
-            self.lista_objetos.append(objeto)
+            if objeto.ng_prest in self.dicionario_chave_num_prestes.keys():
+                print("ng_prest: ",objeto.ng_prest)
+                self.dicionario_chave_num_prestes[objeto.ng_prest].append(objeto)
+            else:
+                lista_aux = []
+                lista_aux.append(objeto)
+                self.dicionario_chave_num_prestes[objeto.ng_prest] = lista_aux
+
 
     # IMPRIMI A DADOS DE DADOS PARA TESTE DOS DADOS
-    def imprimir_tupla_dados(self):
-        contador = 0
-        for obj in self.lista_objetos:
-            contador += 1
-            print("\nPosição da lista: ",contador)
-            print(obj.__dict__)
-            print("")
+    # def imprimir_tupla_dados(self):
+    #     contador = 0
+    #     for obj in self.lista_objetos:
+    #         contador += 1
+    #         print("\nPosição da lista: ",contador)
+    #         print(obj.__dict__)
+    #         print("")
+
+    def imprimir_dicionario(self):
+        lista_num_prestes = self.dicionario_chave_num_prestes.keys()
+        print("\nInicio da lista de chaves: -->{}<-- final da lista!\n".format(lista_num_prestes))
+        for pos in lista_num_prestes:
+            print("Imprimindo a lista para a chave {}".format(pos))
+            lista = self.dicionario_chave_num_prestes[pos]
+            print("lista dessa chave: --->{")
+            for x in lista:
+                print(x.__dict__)
+            print(")}<---")
+            print("-----------------------------------")
+        print("Final da impressão!!!!!")
+
 
     # PASSA POR TODOS OS ARQUIVOS
     def varre_todos_arquivos(self):
         for nome in self.nome_arquivos:
-            self.monta_tupla_de_objetos(nome)
-            self.imprimir_tupla_dados()
+            self.monta_diacionario_de_objetos(nome)
+            # self.imprimir_tupla_dados()
 
 
     def executar_extracao_dados(self):
         self.coleta_nome_arquivos()
         self.varre_todos_arquivos()
+        self.imprimir_dicionario()
 
 
-a = Extracao_dados()
-a.executar_extracao_dados()
