62e8ae6be9d9572fc071facad7ccda88886ff0b2
Wands Anjinho <wandersom.m.viana@gmail.com>
2020-02-29 17:54:15 -0300

    Bot de coleta de planilia nos sites, toda parte de downloads

--- View/main.py
+++ View/main.py
@@ -1,7 +1,7 @@
 
 from Controller.GlosaMax import GlosaMax
-a = GlosaMax('https://glosamax.zeroglosa.com.br', True,'pagatudo')
-a.donwload_arquivos(convenio='pagatudo')
+a = GlosaMax('https://glosamax.zeroglosa.com.br', True,'glosamin')
+a.donwload_arquivos(convenio='glosamin')
 
 # 'https://glosamax.zeroglosa.com.br/glosamax/arquivo/download?nome=glosamax_2018-02-15.xml&data=2018-02-15'
 # 'href="../arquivo/download?nome=glosamax_2018-02-15.xml&data=2018-02-15"'
